da10d089a3c8bb5ba12e6bf911088dada83bee02
Cody Tompkins <112680031+TompyxP@users.noreply.github.com>
2026-03-05 20:33:25 -0700

    day 5 complete

--- src/day05/solution.ts
+++ src/day05/solution.ts
@@ -1,6 +1,6 @@
 import { readLines } from '../utils';
 
-// Returns a map where the key is a page number and the value is an array of page numbers that must come before it in the order
+// Returns a map where the key is a page number and the value is an array of page numbers that must come before it
 const getPageOrderMap = (rules: string[]): Map<number, number[]> => {
     const orderMap: Map<number, number[]> = new Map();
 
@@ -14,32 +14,52 @@ const getPageOrderMap = (rules: string[]): Map<number, number[]> => {
     return orderMap;
 }
 
-const getMiddlePageNumberIfUpdateValid = (orderMap: Map<number, number[]>, update: string): number => {
-    const updates: number[] = update.split(',').map(Number);
-
+const updateIsValid = (orderMap: Map<number, number[]>, updates: number[]): boolean => {
     for (let i = 0; i < updates.length - 1; i++) {
         const currentPage: number = updates[i];
         
         // If the array at the key of the current page number includes any page numbers that appear after it in the updates, 
-        // it means the order is invalid (the array contains page numbers that must come before it)
+        // it means the order is invalid (the array contains page numbers that must come before the key)
         for (let j = i + 1; j < updates.length; j++) {
-            if (orderMap.get(currentPage)!.includes(updates[j])) {
-                return 0;
+            if (orderMap.has(currentPage) && orderMap.get(currentPage)!.includes(updates[j])) {
+                return false;
             }
         }
     }
 
-    return updates[Math.floor(updates.length / 2)];
+    return true;
 }
 
-const getSumOfValidMiddlePageNumbers = (orderMap: Map<number, number[]>, updates: string[]): number => {
+const getSumOfMiddlePageNumbers = (updates: number[][]): number => {
     let sum: number = 0;
 
     for (const update of updates) {
-        sum += getMiddlePageNumberIfUpdateValid(orderMap, update);
+        sum += update[Math.floor(update.length / 2)];
     }
 
     return sum;
+}
+
+const fixInvalidUpdates = (orderMap: Map<number, number[]>, updates: number[][]): number[][] => {
+    const fixedUpdates: number[][] = [];
+
+    for (const update of updates) {
+        const sortedUpdates: number[] = update.sort((a, b) => {
+            const mustComeBeforeA: number[] = orderMap!.get(a) || [];
+            const mustComeBeforeB: number[] = orderMap!.get(b) || [];
+            
+            if (mustComeBeforeA!.includes(b)) {
+                return 1;
+            } else if (mustComeBeforeB!.includes(a)) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+        fixedUpdates.push(sortedUpdates);
+    }
+
+    return fixedUpdates;
 }
 
 const main = () => {
@@ -49,12 +69,21 @@ const main = () => {
         let separatorIndex: number = input.findIndex(line => line === '');
 
         const pageOrderingRules: string[] = input.slice(0, separatorIndex);
-        const updatePageNumbers: string[] = input.slice(separatorIndex + 1);
+        const updatePageNumbers: number[][] = input.slice(separatorIndex + 1).map(line => line.split(',').map(Number));
 
         const pageOrderMap: Map<number, number[]> = getPageOrderMap(pageOrderingRules);
-        const sumOfValidMiddlePageNumbers: number = getSumOfValidMiddlePageNumbers(pageOrderMap, updatePageNumbers);
+
+        const validUpdates: number[][] = updatePageNumbers.filter(update => updateIsValid(pageOrderMap, update));
+        const invalidUpdates: number[][] = updatePageNumbers.filter(update => !updateIsValid(pageOrderMap, update));
+        
+        const sumOfValidMiddlePageNumbers: number = getSumOfMiddlePageNumbers(validUpdates);
+
+        const fixedInvalidUpdates: number[][] = fixInvalidUpdates(pageOrderMap, invalidUpdates);
+        const sumOfFixedMiddlePageNumbers: number = getSumOfMiddlePageNumbers(fixedInvalidUpdates);
+
 
         console.log('Sum of valid middle page numbers:', sumOfValidMiddlePageNumbers);
+        console.log('Sum of invalid middle page numbers after fix:', sumOfFixedMiddlePageNumbers);
 
     } catch (error) {
         console.error('Error:', error);
